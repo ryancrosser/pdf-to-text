fe848a39d99702ae2942d93db267fcf78ca6a73f
Ryan Crosser <crosser_ryan@bah.com>
2016-03-08 09:14:07 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var Promise = require('bluebird');
+require('./lib/pdf.js');
+require('./lib/compatibility.js');
+
+module.exports = function(input) {
+    return new Promise(function (resolve, reject) {
+        PDFJS.getDocument(input).then((pdfDocument) => {
+            for (let pageNum = 1; pageNum <= pdfDocument.numPages; pageNum++) {
+                pdfDocument.getPage(pageNum).then((page) => {
+                    page.getTextContent().then((textContent) => {
+                        let pageText = [];
+                        textContent.items.forEach((textItem) => {
+                            pageText.push(textItem.str);
+                        });
+                        pageText.join(' ');
+
+                        docText[pageNum] = pageText;
+                        if (Object.keys(docText).length === pdfDocument.numPages) {
+                            // we are done
+                            let docArr = [];
+                            for (pageNum = 1; pageNum <= pdfDocument.numPages; pageNum++) {
+                                docArr.push(docText[pageNum]);
+                            }
+                            let text = docArr.join('\n\n');
+                            resolve(text);
+                        }
+                    });
+                });
+            }
+        });
+    });
+};
